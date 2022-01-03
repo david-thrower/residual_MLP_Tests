7e14f562a2cfc86365eec065a724c6ba0bf61c1a
David Thrower <david.thrower@outlook.com>
2022-01-03 01:00:12 -0500

    Archived test run content.

---
+++ archives_2/archives/old_runs/2021-12-19_17_26_INCREASED_MLP_COMPLEXITY_COMPLETED/run_2021-12-19_17_23_job.sh
@@ -0,0 +1,4 @@
+echo "Same run but last conv2d not trainable" >> 2021-12-19_17_23_lr10e-3_bl5_7fl5_flaten_bnorm_python3_shell_log.txt
+pip3 install pandas
+pip3 install numpy
+python3 -u task.py --project_name "tandem-EfficientNetB7-ResidualMLP" --results_dir "2021-12-19_17_23_lr10e-3_bl5_7fl5_flaten_bnorm_results" --best_model_dir "2021-12-19_17_23_exported_model_lr10e-3_bl5_7fl5_flaten_bnorm" --epochs 150 --patience 80 --patience_min_delta 0.00001 --training_set_size 5000 --batch_size 50 --eval_size 50 --learning_rate .0007 >> 2021-12-19_17_23_lr10e-3_bl5_7fl5_flaten_bnorm_python3_shell_log.txt
